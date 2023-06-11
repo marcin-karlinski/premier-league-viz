46d71c4472ef8d0d053cd61e2fbaf0313bad770a
marcin-karlinski <marcin.karlinski5@gmail.com>
2023-06-11 08:30:54 +0200

    Added tablerDash library

--- app.R
+++ app.R
@@ -2,11 +2,12 @@ library(shiny)
 library(worldfootballR)
 library(tidyverse)
 library(highcharter)
+library(tablerDash)
 
-stats_shooting <- fb_big5_advanced_season_stats(season_end_year= "2023", stat_type= "shooting", team_or_player= "team") %>% 
+stats_shooting <- fb_big5_advanced_season_stats(season_end_year = "2023", stat_type= "shooting", team_or_player= "team") %>% 
   filter(Comp == "Premier League" & Team_or_Opponent == "team")
 
-stats <- fb_big5_advanced_season_stats(season_end_year= "2023", stat_type= "standard", team_or_player= "team") %>% 
+stats <- fb_big5_advanced_season_stats(season_end_year= "2023", stat_type= "standard", team_or_player = "team") %>% 
   filter(Comp == "Premier League")
 
 ui <- fluidPage(
@@ -16,9 +17,9 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   output$hc1 <- renderHighchart({
     stats %>% 
-      arrange(-`np:G_minus_xG_Expected`) %>% 
+      arrange(-`npxG_Per`) %>% 
       hchart(
-      'bar', hcaes(x = Squad, y = `np:G_minus_xG_Expected`),
+      'bar', hcaes(x = Squad, y = `npxG_Per`),
       color = "#0073C2FF"
     )
       
